f52f1c7d9509f0b00676ca05323cae9acb521f6b
Paula Yuan <ypaula521@gmail.com>
2019-12-21 13:01:20 -0500

    Created CCC'00J3
    Slot Machines

---
+++ CCC'00J3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SlotMachines{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int turns = 0;
+        int numQuarters = input.nextInt();
+        int timesSincePayed1 = input.nextInt();
+        int timesSincePayed2 = input.nextInt();
+        int timesSincePayed3 = input.nextInt();
+        while (numQuarters > 0) {
+            numQuarters--;
+            timesSincePayed1++;
+            if (timesSincePayed1 == 35) {
+                numQuarters += 30;
+                timesSincePayed1 = 0;
+            }
+            turns++;
+            if (numQuarters == 0) {
+                break;
+            }
+            numQuarters--;
+            timesSincePayed2++;
+            if (timesSincePayed2 == 100) {
+                numQuarters += 60;
+                timesSincePayed2 = 0;
+            }
+            turns++;
+            if (numQuarters == 0) {
+                break;
+            }
+            numQuarters--;
+            timesSincePayed3++;
+            if (timesSincePayed3 == 10) {
+                numQuarters += 9;
+                timesSincePayed3 = 0;
+            }
+            turns++;
+            if (numQuarters == 0) {
+                break;
+            }
+        }
+        System.out.println("Martha plays " + turns + " times before going broke.");
+    }
+}
